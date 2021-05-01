16e903eb916fe7b03cd53b72e800f493a09e1290
Patrick Bolden <boldenpm@gmail.com>
2021-05-01 19:14:37 -0400

    Adding Spring Boot dependencies for command line runner support

---
+++ src/main/java/com/nextiteration/kidexercises/KidExercisesApplication.java
@@ -0,0 +1,13 @@
+package com.nextiteration.kidexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KidExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KidExercisesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nextiteration/kidexercises/KidExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextiteration.kidexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KidExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

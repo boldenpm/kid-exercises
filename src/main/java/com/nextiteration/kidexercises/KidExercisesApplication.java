9596af5c5554c79d10be93809ace76781d6a47f9
Patrick Bolden <boldenpm@gmail.com>
2021-05-04 09:37:35 -0400

    Updating .gitignore and changing the applicaiton to a commandlinerunner showing a Hello World application

--- src/main/java/com/nextiteration/kidexercises/KidExercisesApplication.java
+++ src/main/java/com/nextiteration/kidexercises/KidExercisesApplication.java
@@ -1,13 +1,19 @@
 package com.nextiteration.kidexercises;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KidExercisesApplication {
+public class KidExercisesApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KidExercisesApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		
+		System.out.println("Hello World!");
+	}
 }

05c45f6d0dffb9dc5fc595903e95f32b476bd841
Patrick Bolden <boldenpm@gmail.com>
2021-05-14 12:22:38 -0400

    Added standalone classes which demonstrate basic structures and control structures

--- src/main/basics/SimpleLoop.java
+++ src/main/basics/SimpleLoop.java
@@ -5,6 +5,5 @@ public class SimpleLoop {
         for (int i = 0; i < 10; i++) {
             System.out.println("Hello World! #" + i);
         }
-
     }
 }
--- src/main/basics/Variables.java
+++ src/main/basics/Variables.java
@@ -3,11 +3,11 @@ public class Variables {
     public static void main(String[] args) {
 
         int integer = 1;
-        boolean trueOrFalse = true;
+        boolean trueOrFalse = false;
         String text = "This is a String.  It can have whole numbers (integers) like [15], decimals (floating point) like [1.56] and special characters like [@#$^].";
         int[] integerArray = new int[10];
-        
-        integerArray[0] = 1;
+
+        integerArray[0] = 2;
         integerArray[1] = 3;
         integerArray[2] = 4;
         integerArray[3] = 9;
@@ -23,7 +23,6 @@ public class Variables {
         System.out.println(text);
         System.out.println(integerArray);
 
-        System.out.println("Do you think a quotation mark can be put in a String since it is surrounded by quotes?");
-
+        System.out.println("Do you think a quotation mark can be \"put\" in a String since it is surrounded by quotes?");
     }
 }
